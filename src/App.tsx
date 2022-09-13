4434ef6af81310c5e8528d141bc589ba76ea3f65
Santiago Torres Aragon <santiagoatorres14@gmail.com>
2022-09-12 20:01:17 -0500

    Main project parts finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import {useState} from 'react';
 import './App.css';
 import DevelopmentTeamSection from "./components/Sections/DevelopmentTeamSection";
+import Card from "./components/UI/Card";
+import ProjectsSection from "./components/Sections/ProjectsSection";
 
 function App() {
-
 
     return (
         <>
             <DevelopmentTeamSection/>
-
+            <ProjectsSection/>
         </>
     );
 }
